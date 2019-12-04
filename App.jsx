d12ad5892aaba46e8098dd5a8b60849da7559807
Daniel <danielevalberg@gmail.com>
2019-12-04 23:26:26 +0000

    removed load contact on load

--- App.jsx
+++ App.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 
 import AppContainer from './src/routes';
-import { contactsInit } from './src/services/contactFileService';
 
-const App = () => {
-	React.useEffect(() => {
-		(async () => {
-			await contactsInit();
-		})();
-	}, []);
-
-	return (
-		<AppContainer />
-	);
-};
-
+const App = () => (
+	<AppContainer />
+);
 
 export default App;
